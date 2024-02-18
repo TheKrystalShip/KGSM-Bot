50f622f7ee88ff3ac2a6feb7f9981554688405bd
Cristian Moraru <cristian.moraru@live.com>
2024-02-18 17:54:09 +0100

    Moved server script files to different repo

--- home/cristian/servers/7dtd/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WORKING_DIR=/home/"$USER"/servers/7dtd/install
-CONFIG_FILE="$WORKING_DIR"/serverconfig.xml
-
-exec "$WORKING_DIR"/7DaysToDieServer.x86_64 -quit -batchmode -nographics -headless -dedicated -configfile="$CONFIG_FILE"
