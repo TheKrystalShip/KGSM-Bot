897b0245301b6b1b3b6d0eb8d03a4fa28c362641
Cristian Moraru <cristian.moraru@live.com>
2024-11-06 21:21:03 +0100

    Added /install command with dynamic list of blueprints

--- KGSM/Modules/ServerModule.cs
+++ KGSM/Modules/ServerModule.cs
@@ -1,4 +1,4 @@
-using Discord;
+﻿using Discord;
 using Discord.Interactions;
 
 using Game = TheKrystalShip.KGSM.Domain.Game;
@@ -128,5 +128,48 @@ public partial class ServerModule : InteractionModuleBase<SocketInteractionConte
     {
         KgsmResult result = _interop.GetIp();
         await RespondAsync(result.Stdout ?? result.Stderr);
+    }
+
+    [SlashCommand("install", "Install a new game server")]
+    public async Task InstallAsync(
+        [Summary(description: "Blueprint to create a new install from")]
+        [Autocomplete(typeof(BlueprintAutocompleteHandler))]
+        string blueprint
+    )
+    {
+        await RespondAsync($"Installing {blueprint}...");
+
+        KgsmResult result = _interop.Install(blueprint);
+        await FollowupAsync(result.Stderr ?? result.Stdout);
+    }
+
+    public class BlueprintAutocompleteHandler : AutocompleteHandler
+    {
+        private readonly List<string> _blueprints = [];
+        private readonly KgsmInterop _interop;
+
+        public BlueprintAutocompleteHandler(KgsmInterop interop)
+        {
+            _interop = interop;
+            KgsmResult result = _interop.GetBlueprints();
+
+            if (result.ExitCode == 0) {
+                _blueprints = result.Stdout?.Split('\n').ToList() ?? [];
+            }
+        }
+
+        public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
+        {
+            string currentInput = autocompleteInteraction.Data.Current.Value?.ToString() ?? string.Empty;
+            
+            // Generate list of best matches
+            IEnumerable<AutocompleteResult> suggestions = _blueprints
+                .Where(choice => choice.StartsWith(currentInput, StringComparison.OrdinalIgnoreCase))
+                .Select(choice => new AutocompleteResult(choice, choice))
+                .ToList();
+
+            // Max 25 suggestions at a time because of Discord API
+            return AutocompletionResult.FromSuccess(suggestions.Take(25));
+        }
     }
 }
