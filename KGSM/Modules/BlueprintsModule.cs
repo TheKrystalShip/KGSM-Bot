0985d76ae0639452c430a970d3a9e5d9cabf4700
Cristian Moraru <cristian.moraru@live.com>
2024-11-08 21:21:37 +0100

    Split slash commands into multiple modules

---
+++ KGSM/Modules/BlueprintsModule.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Interactions;
+
+using Game = TheKrystalShip.KGSM.Domain.Game;
+
+using TheKrystalShip.Logging;
+
+namespace TheKrystalShip.KGSM.Modules;
+
+// Interaction modules must be public and inherit from an IInteractionModuleBase
+public partial class BlueprintsModule : InteractionModuleBase<SocketInteractionContext>
+{
+
+    private readonly KgsmInterop _interop;
+    private readonly Logger<BlueprintsModule> _logger;
+
+    // Constructor injection is also a valid way to access the dependencies
+    public BlueprintsModule(KgsmInterop interop)
+    {
+        _interop = interop;
+        _logger = new();
+    }
+
+    [SlashCommand("install", "Install a new game server")]
+    public async Task InstallAsync(
+        [Summary(description: "Blueprint to create a new install from")]
+        [Autocomplete(typeof(BlueprintAutocompleteHandler))]
+        string blueprint
+    )
+    {
+        await RespondAsync($"Installing {blueprint}...");
+
+        KgsmResult result = _interop.Install(blueprint);
+        await FollowupAsync(result.Stderr ?? result.Stdout);
+    }
+}
---
+++ KGSM/Modules/GeneralModule.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Interactions;
+
+using Game = TheKrystalShip.KGSM.Domain.Game;
+
+using TheKrystalShip.Logging;
+
+namespace TheKrystalShip.KGSM.Modules;
+
+// Interaction modules must be public and inherit from an IInteractionModuleBase
+public partial class GeneralModule : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly KgsmInterop _interop;
+    private readonly Logger<GeneralModule> _logger;
+
+    // Constructor injection is also a valid way to access the dependencies
+    public GeneralModule(KgsmInterop interop)
+    {
+        _interop = interop;
+        _logger = new();
+    }
+
+    [SlashCommand("get-ip", "Get the server's IP address")]
+    public async Task GetIPAsync()
+    {
+        KgsmResult result = _interop.GetIp();
+        await RespondAsync(result.Stdout ?? result.Stderr);
+    }
+}
